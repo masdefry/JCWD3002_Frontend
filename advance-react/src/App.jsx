940ddc36d95732b2cd65fd90605898adae572836
masdefry <muhammaddefryan@gmail.com>
2024-09-23 14:10:39 +0700

    useCallback

--- advance-react/src/App.jsx
+++ advance-react/src/App.jsx
@@ -4,6 +4,8 @@ import HomeClass from './pages/class';
 // Setup React Router DOM
 import { Routes, Route } from 'react-router-dom';
 import HomeFunction from './pages/function';
+import UseRef from './pages/useRef';
+import UseCallback from './pages/useCallback';
 
 function App(){
   return(
@@ -11,6 +13,8 @@ function App(){
       <Routes>
         <Route path='/home-class' element={<HomeClass />} />
         <Route path='/home-function' element={<HomeFunction />} />
+        <Route path='/use-ref' element={<UseRef />} />
+        <Route path='/use-callback' element={<UseCallback />} />
       </Routes>
     </>
   )
